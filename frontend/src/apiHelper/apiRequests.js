90b63d59bb1d1f180d2807449f76458ce3852471
AbhishekKr-Jha <akjha4127@gmail.com>
2025-06-29 02:57:41 +0530

    chnages added

--- frontend/src/apiHelper/apiRequests.js
+++ frontend/src/apiHelper/apiRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BACKENED_URL="http://localhost:3000/api/v1" 
-// const BACKENED_URL=`${import.meta.env.VITE_BACKEND_URL}api/v1` 
+// const BACKENED_URL="http://localhost:3000/api/v1" 
+const BACKENED_URL=`${import.meta.env.VITE_BACKEND_URL}api/v1` 
 
  
 
